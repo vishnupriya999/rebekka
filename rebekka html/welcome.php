d0e5443f84fb688766e40a93a8dfb664bc382c3d
vishnupriya999 <48430722+vishnupriya999@users.noreply.github.com>
2019-03-18 18:07:30 +0530

    Add files via upload

---
+++ rebekka html/welcome.php
@@ -0,0 +1,21 @@
+<html>
+<head>
+<meta charset="utf-8">
+<title>Animation</title>
+<link rel="stylesheet" href="welcome1.css">
+</head>
+<body>
+<form action="login.html">
+<div class="container">
+<span class="text1">welcome in login</span>
+</div>
+<div class="container2">
+</div>
+<div class="sumbit">
+<a class="btn"  href="login.php">back</a>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
+<a class="btn"  href="">Next</a>
+</div>
+</div>
+</form>
+</body>
+</html>
